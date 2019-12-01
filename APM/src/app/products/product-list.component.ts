0ad42fe50815b06aa2e7837813a8c8868dcb6de5
Martin[S] <mortnmx@hotmail.com>
2019-12-01 19:48:59 +0100

    FinishedCourse

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,39 +1,63 @@
-import { Component } from "@angular/core";
+import { IProduct } from './product';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from './product.service';
 
 @Component({
-    selector: "pm-products",
-    templateUrl: "./product-list.component.html"
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
-    products: any[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          },
-    ];
+    // listFilter: string = "cart";
 
+    _listFilter: string;
+    public get listFilter() : string {
+        return this._listFilter;
+    }
+    public set listFilter(value : string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService) {
+        // this.listFilter = "cart";
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+    
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log("In OnInit");
+        // this.products = this.productService.getProducts();
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products,
+                this.filteredProducts = this.products;
+            },
+            // error: err => this.errorMessage = err
+            // next(products) { 
+            //     this.products = products,
+            //     this.filteredProducts = this.products;}, // doesn´t work.... don´t know why
+            error(err) { this.errorMessage = err }
+        });
+        
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = "Product List: " + message;
+        console.log("Notify trigger");
     }
 }
